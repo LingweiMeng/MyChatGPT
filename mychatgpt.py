167f83a759f785bb35b367435fc650e104217a97
Lingwei Meng <lingw.meng@gmail.com>
2023-10-14 19:24:26 +0800

    add temperature param

--- mychatgpt.py
+++ mychatgpt.py
@@ -5,8 +5,9 @@ import readline
 # Choose Azure API or OpenAI API, comment out another one.
 
 # Azure API
+# ref: https://learn.microsoft.com/en-us/azure/ai-services/openai/reference
 openai.api_type = "azure"
-openai.api_version = "2023-03-15-preview"
+openai.api_version = "2023-05-15"
 openai.api_base = "INPUT_YOUR_ENDPOINT_URL"
 openai.api_key = "INPUT_YOUR_API_KEY"
 deployment_name = {"engine": "INPUT_YOUR_MODEL_NAME"}
@@ -20,6 +21,12 @@ deployment_name = {"engine": "INPUT_YOUR_MODEL_NAME"}
 # Some casual prompt here.
 prompt = "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. "
 # prompt += "Respond using markdown."
+
+creation_params = {
+    "temperature": 1.0,
+    "frequency_penalty": 0.0,
+    "presence_penalty": 0.0,
+}
 
 
 RED = "\033[91m"
@@ -39,10 +46,14 @@ INFO_COLOR = PURPLE
 
 class MyChatGPT:
     def __init__(self, args):
-        self.save_on_the_fly = args.save
         self.conversation = []
         self.conversation_init = []
-
+        self.save_on_the_fly = args.save
+        
+        self.temperature = args.temperature
+        self.frequency_penalty = args.frequency_penalty
+        self.presence_penalty = args.presence_penalty
+        
         if args.load is not None:
             self.load_from_file(args.load)
         else:
@@ -68,6 +79,10 @@ class MyChatGPT:
                 conversation = []
                 role = None
                 content = ""
+                # load temperature
+                if lines[0].split()[0] == "temperature:": 
+                    self.temperature = float(lines[0].split()[1])
+                    lines = lines[2:]
                 for line in lines:
                     if line.strip() in ["user:", "assistant:", "system:"]:
                         if role is not None:
@@ -80,16 +95,19 @@ class MyChatGPT:
                     conversation.append({"role": role, "content": content.strip()})
                 self.conversation = conversation
             self.print_history()
-            print(INFO_COLOR + "File loaded.\n" + END)
+            print(INFO_COLOR + "Current temperature: " + str(self.temperature))
+            print("File loaded.\n" + END)
         except FileNotFoundError:
             print(ERROR_COLOR + "File not found.\n" + END)
 
-    def save_to_file(self, file_name):
+    def save_to_file(self, file_name, is_cmd=True):
         try:
             lines = [f"{message['role']}:\n{message['content']}\n\n" for message in self.conversation]
             with open(file_name, "w") as f:
+                f.write(f"temperature: {self.temperature}\n\n")
                 f.writelines(lines)
-            print(INFO_COLOR + "Conversation history saved.\n" + END)
+            if is_cmd:
+                print(INFO_COLOR + "Conversation history saved.\n" + END)
         except FileNotFoundError:
             print(ERROR_COLOR + "Error: Conversation history not saved. It may be a path error.\n" + END)
 
@@ -113,12 +131,18 @@ class MyChatGPT:
                 self.conversation = self.conversation[:-2] if len(self.conversation) > 2 else self.conversation_init
                 self.print_history()
 
+            elif user_input == "temperature":
+                print(INFO_COLOR + "Current temperature: " + str(self.temperature) + END)
+                print(INFO_COLOR + "Please input a new temperature value: " + END)
+                self.temperature = float(input())
+                print()
+
             elif len(user_input.split()) == 2 and user_input.split()[0] == "load":
                 self.load_from_file(user_input.split()[1])
 
             elif len(user_input.split()) == 2 and user_input.split()[0] == "save":
                 self.save_to_file(user_input.split()[1])
-
+            
             elif user_input == "help":
                 print(INFO_COLOR + "\n" + "----" * 10)
                 print("HELP:")
@@ -126,8 +150,9 @@ class MyChatGPT:
                 print("clear: clear the conversation history.")
                 print("history: show the conversation history.")
                 print("back: back to the previous conversation.")
-                print("load [file_name]: load the conversation history from a file.")
-                print("save [file_name]: save the conversation history to a file.")
+                print("temperature: check and change the temperature.")
+                print("load FILE_PATH: load the conversation history from a file.")
+                print("save FILE_PATH: save the conversation history to a file.")
                 print("help: show the help message.")
                 print("----" * 10 + END + "\n")
 
@@ -141,6 +166,9 @@ class MyChatGPT:
             try:
                 response = openai.ChatCompletion.create(
                     messages=self.conversation,
+                    temperature=self.temperature,
+                    frequency_penalty=self.frequency_penalty,
+                    presence_penalty=self.presence_penalty,
                     **deployment_name,
                     request_timeout=120
                 )
@@ -154,7 +182,7 @@ class MyChatGPT:
             print("\n" + ASSIS_COLOR + 'assistant: \n' + response['choices'][0]['message']['content'] + END + "\n")
 
             if self.save_on_the_fly is not None:
-                self.save_to_file(self.save_on_the_fly)
+                self.save_to_file(self.save_on_the_fly, is_cmd)
 
 
 if __name__ == "__main__":
@@ -162,6 +190,16 @@ if __name__ == "__main__":
     arg_parser.add_argument("--prompt", type=str, default=None)
     arg_parser.add_argument("--load", type=str, default=None)
     arg_parser.add_argument("--save", type=str, default=None)
+    arg_parser.add_argument("--temperature", type=float, default=creation_params["temperature"],
+                            help="The higher the value, the random the text.")
+    arg_parser.add_argument("--frequency_penalty", type=float, default=creation_params["frequency_penalty"],
+                            help="The higher the value, the less repetitive text.")
+    arg_parser.add_argument("--presence_penalty", type=float, default=creation_params["presence_penalty"], 
+                            help="The higher the value, the more likely the model will talk about new topics.")
+    # arg_parser.add_argument("--max_tokens", type=int, default=creation_params["max_tokens"],
+    #                         help="The maximum number of tokens to generate.")
+    # arg_parser.add_argument("--n", type=int, default=1,
+    #                         help="How many chat completion choices to generate for each input message.")
     args = arg_parser.parse_args()
 
     args.prompt = prompt + "\n" + args.prompt if args.prompt is not None else prompt
